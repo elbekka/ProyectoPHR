6ae4ab0d5c0acfab142d3464a9524d506eddc0cb
Serigoto <39056072+Serigoto@users.noreply.github.com>
2018-05-31 10:01:23 +0200

    Update entityDetector.vhd

--- vhdl/entityDetector.vhd
+++ vhdl/entityDetector.vhd
@@ -13,7 +13,7 @@ end entity;
 architecture arquitectura of entityDetection is
  
     signal auxCorrect0,auxCorrect1,auxCorrect2,auxCorrect3,auxCorrect4,auxCorrect5,auxCorrect6:std_logic;
-    signal interrupcion: std_logic;
+    signal interruption: std_logic;
     type state_type is (E,N,T,I,F,Y,ESPACIO);
     signal state : state_type := E ;
 
@@ -25,8 +25,7 @@ architecture arquitectura of entityDetection is
   ) ;
     end component;
 
-begin
---LLAMADAS A LAS ENTIDADES
+begin 
 
 comp1 : memCompare port map (bitInput,4,isCorrect=>auxCorrect0); --E
 comp2 : memCompare port map (bitInput,13,isCorrect=>auxCorrect1);--N
@@ -34,62 +33,58 @@ comp3 : memCompare port map (bitInput,19,isCorrect=>auxCorrect2);--T
 comp4 : memCompare port map (bitInput,8,isCorrect=>auxCorrect3); --I
 comp5 : memCompare port map (bitInput,19,isCorrect=>auxCorrect4);--T
 comp6 : memCompare port map (bitInput,24,isCorrect=>auxCorrect5);--Y
-comp7 : memCompare port map (bitInput,26, isCorrect=>auxCorrect6);--(BLANCO)
+comp7 : memCompare port map (bitInput,26,isCorrect=>auxCorrect6);--(BLANCO)
 process (clk)
     begin
-	if(reset='0' or interrupcion='1') then
+	if(reset='0' or interruption='1') then
 		detectedBit <='0' ;
         	--state <= A;
 	elsif(rising_edge(clk)) then
 		case state is
 
 			when E=>
-                		detectedBit <='0' ;
-                		if(auxCorrect0 = '1') then
-                 		   state <=N;
-				else
-                    		   interrupcion<='1';
-               			end if;
+                	detectedBit <='0' ;
+                	if(auxCorrect0 = '1') then state <=N;
+			else interruption<='1';
+               		end if;
+
+
 			when N=>
-				if(auxCorrect1 = '1') then
-                 		   state <=T;
-				else
-                    		    interrupcion<='1';
-               			end if;
-			when T=>
-				if(auxCorrect2 = '1') then
-                 		   state <=I;
-				else
-                    		   interrupcion<='1';
-               			end if;
+			if(auxCorrect1 = '1') then state <=T;
+			else interruption<='1';
+               		end if;
+
+
+			when T=> 
+			if(auxCorrect2 = '1') then state <=I;
+			else interruption<='1';
+               		end if;
+
+
 			when I=>
-				if(auxCorrect3 = '1') then
-                 		   state <=F;
-				else
-                    		   interrupcion<='1';
-               			end if;
+			if(auxCorrect3 = '1') then state <=F;
+			else interruption<='1';
+               		end if;
+
+
 			when F=>
-				if(auxCorrect4 = '1') then
-                 		   state <=Y;
-				else
-                    		   interrupcion<='1';
-               			end if;
-			when Y=>
-				if(auxCorrect5 = '1') then
-                 		   state <= ESPACIO;
-				else
-                    		   interrupcion<='1';
-               			end if;
+			if(auxCorrect4 = '1') then state <=Y;
+			else interruption<='1';
+               		end if;
+
+
+			when Y=> 
+			if(auxCorrect5 = '1') then state <= ESPACIO;
+			else interruption<='1';
+               		end if;
+
+
 			when ESPACIO=>
-				if(auxCorrect6 = '1') then
-                       		  detectedBit <= '1' ;
-                       		  --state <=E;
-                   		else
-                      		  interrupcion<='1';
-                    		end if;
+			if(auxCorrect6 = '1') then detectedBit <= '1';
+                	else interruption<='1';
+                	end if;
 			
-			when others=>
-				interrupcion<='1';
+			when others=> NULL;	
 		end case;
 	end if;
    end process;
