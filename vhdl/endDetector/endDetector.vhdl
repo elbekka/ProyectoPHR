42cf43379e4493d76ceb3fff27f94eea858f68af
Serigoto <39056072+Serigoto@users.noreply.github.com>
2018-05-31 10:03:12 +0200

    Update endDetector.vhdl

--- vhdl/endDetector/endDetector.vhdl
+++ vhdl/endDetector/endDetector.vhdl
@@ -11,14 +11,15 @@ entity end_Detector is
 end end_Detector;
 
 architecture Behavioral of end_Detector is
-type state_type is (A,B,C);
-    signal state : state_type := A ;
-    signal auxBit2,auxBit1,auxBit : std_logic;
-    
+type state_type is (E,N,D,ESPACIO);
+    signal state : state_type := E ;
+    signal auxBit3,auxBit2,auxBit1,auxBit : std_logic;
+    signal interruption: std_logic;
+
     component memCompare is
         port (
           charInput : in std_logic_vector(7 downto 0);
-          address   : in integer range 0 to 25 ;
+          address   : in integer range 0 to 30 ;
           isCorrect : out std_logic
         ) ;
       end component;
@@ -26,33 +27,39 @@ begin
     memCompare1: memCompare port map (bitInput,4,isCorrect=>auxBit);
     memCompare2: memCompare port map (bitInput,13,isCorrect=>auxBit1);
     memCompare3: memCompare port map (bitInput,3,isCorrect=>auxBit2);
+    memCompare4: memCompare port map (bitInput,26,isCorrect=>auxBit3);
     process (clk)
     begin   
-        if(reset = '1') then 
-        detectedBit <='0' ;
-        state <= A;
+        if(reset = '1' or interruption = '1') then 
+        detectedBit <='0';
         elsif(rising_edge(clk)) then
             case state is
-                when A =>
+
+
+                when E =>
                 detectedBit <='0' ;
-                if(auxBit = '1') then
-                    state <=B;
-                else
-                    state <=A;
+                if(auxBit = '1') then state <=N;
+                else interruption <= '1';
                 end if;
-                when B =>
-                if(auxBit1 = '1') then
-                        state <=C ;
-                    else
-                        state <= A;
-                    end if; 
-                when C =>
-                if(auxBit2 = '1') then
-                    detectedBit <='1' ;
-                    state <=A;
-                else
-                    state <=A;
+
+
+                when N =>
+                if(auxBit1 = '1') then state <=D ;
+                else interruption <= '1';
+                end if; 
+
+
+                when D =>
+                if(auxBit2 = '1') then state <=ESPACIO;
+                else interruption <= '1';
                 end if;
+
+
+                when ESPACIO =>
+                if(auxBit3= '1') then  detectedBit <='1';
+                else interruption <= '1';
+                end if;
+                    
                 when others => NULL;
             end case;
         end if;
