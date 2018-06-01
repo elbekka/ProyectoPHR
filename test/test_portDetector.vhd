b4a4b5365c2eb855b4620c9c25ac717c2458c3ea
Serigoto <39056072+Serigoto@users.noreply.github.com>
2018-06-01 11:19:03 +0200

    Update test_portDetector.vhd

--- test/test_portDetector.vhd
+++ test/test_portDetector.vhd
@@ -1,94 +1,65 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
 
-library ieee;
+ENTITY tb_portDetector IS
+END tb_portDetector;
 
-use ieee.std_logic_1164.all;
+ARCHITECTURE behavior OF tb_portDetector IS
 
-
-
-entity test_portDetection is
-
-end entity;
-
-
-
-architecture behavior of test_portDetection is
-
-     signal clk,reset,detectedBit : std_logic := '0';
-
-     signal bitInput : std_logic_vector(7 downto 0) := x"00";
-
-     constant clk_period : time := 10 ns;
-
-     component port_DetectorESpacio is
-
-     port (
-
+   signal clk,reset,detectedBit : std_logic := '0';
+   signal bitInput : std_logic_vector(7 downto 0) := x"00";
+   constant clk_period : time := 10 ns;
+   component portDetection is
+    port (
       clk: in std_logic;
-
-     reset : in std_logic;
-
+      reset : in std_logic;
       bitInput : in std_logic_vector(7 downto 0);
-
       detectedBit : out std_logic -- '1' si ha detectado la secuencia.
-
     ) ;
-
   end component;
 
-
-
 BEGIN
+ detector: portDetection port map (clk,reset,bitInput,detectedBit);
+    -- Instantiate the Unit Under Test (UUT)
+   --uut: is_Detector PORT MAP (
+     --     clk => clk,
+       --   reset => reset,
+         -- bitInput => bitInput,
+          --detectedBit => detectedBit
+        --);
 
- detector:  port_DetectorESpacio port map (clk,reset,bitInput,detectedBit);
-
-
-
-clk_process :process
-
+   -- Clock process definitions
+   clk_process :process
    begin
-
         clk <= '0';
-
         wait for clk_period/2;
-
         clk <= '1';
-
         wait for clk_period/2;
-
    end process;
 
-   
-
-
-
+   -- Stimulus process     : Apply the bits in the sequence one by one.
    stim_proc: process
+   begin      
+        bitInput <= x"70";             --P
+        wait for clk_period;
 
-   begin       
+        bitInput <= x"6F";             --O
+        wait for clk_period;
 
-      bitInput <= x"6F";             --P
+        bitInput <= x"72";             --R
+        wait for clk_period;
 
-      wait for clk_period;
-
-      bitInput <= x"6E";             --O
-
-      wait for clk_period;
-
-      bitInput <= x"71";             --R
-
-      wait for clk_period;
-
-	bitInput <= x"73";             --T
-
-      wait for clk_period;
-       
-         bitInput<= x"32";      --Espacio
-
-       wait for clk_period;
-
-
-      wait;        
-
+        bitInput <= x"74";             --T
+        wait for clk_period;
+    
+        bitInput<= x"32";
+        wait for clk_period;  
+        
+        bitInput <= x"32";           --110111
+        wait for clk_period;
+        
+        bitInput <= x"10";           --1101110
+        wait for clk_period;
+	wait;
    end process;
-
-
 END;
