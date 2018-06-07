60f1b29b768d8f0d90d58e63278f8ee5089e4953
elbekka <hakimelbekka@gmail.com>
2018-06-07 12:13:01 +0200

    .

--- pruebaRendimiento/prueba.cpp
+++ pruebaRendimiento/prueba.cpp
@@ -5,16 +5,16 @@
 int main(int argc, char const *argv[])
 {
     /* code */
-    char letraE,letraN,letraD;
-    std::string cadena= "end";
+    char cadena[3] = {'e','n','d'};
+    bool enc = false;
     unsigned t0,t1;
     t0=clock();
     if(cadena[0]=='e')
         if(cadena[1]=='n')
             if(cadena[2]=='d')
-                std::cout << "end correcto";
+                bool enc = true;
     t1=clock();
     double time = (double(t1-t0)/CLOCKS_PER_SEC);
-    std::cout << "Execution Time: " << time;
+    std::cout << "Execution Time: " << time << " bool : " << enc;
     return 0;
 }
